f3659253bbe97287b4d1502d329342448db1c5af
plainnany <plainnany@gmail.com>
2017-11-23 17:10:43 +0800

    cpmplete tab component

--- js/dom.js
+++ js/dom.js
@@ -15,5 +15,38 @@ var dom = {
             parent.appendChild(children[i])
         }
         
+    },
+    on: function(element,eventType,selector,fn){
+        element.addEventListener(eventType,e=>{
+            let ele = e.target
+            while(!ele.matches(selector)){
+                if(ele === element){
+                    ele = null
+                    break
+                }
+                ele = ele.parentNode
+            }
+
+            ele && fn.call(ele,e,ele)
+            
+        })
+        return element
+    },
+    uniqueClass: function(element,className){
+        let siblings = element.parentNode.children
+        for(let i=0;i<siblings.length;i++){
+            siblings[i].classList.remove(className)
+        }
+        element.classList.add(className)
+    },
+    index: function(element){
+        let ele = element.parentNode.children
+        for(let i=0;i<ele.length;i++){
+            if(element === ele[i]){
+                return i
+            }
+        }
+        return -1
     }
+
 }
