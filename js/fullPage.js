af9fedd6ef98deeb5aae187055a7b26e1cea8b39
plainnany <plainnany@gmail.com>
2017-11-24 16:27:33 +0800

    use promise to complete fullpage

--- js/fullPage.js
+++ js/fullPage.js
@@ -27,33 +27,59 @@ class FullPage{
         this.options.element.addEventListener('wheel',e => {
             let index = e.deltaY > 0 ? 1 : -1
             let targetIndex = this.currentIndex + index
-            if(this.animate){return }
-            this.gotoNextSection(targetIndex)  // 
+            this.gotoNextSection(targetIndex).then(
+                () => {
+                    this.currentIndex = targetIndex
+                },
+                () => {}
+            )
             
         })
     }
     gotoNextSection(targetIndex){
-        this.animate = true
-        this.currentIndex = targetIndex
-        let sections = this.options.element.children
-        let _this = this
+        // this.animate = true
+        // this.currentIndex = targetIndex
+        // let sections = this.options.element.children
+        // let _this = this
 
-        if(this.currentIndex < 0){
-            this.currentIndex = 0
-            this.animate = false
-        }
-        if(this.currentIndex >= sections.length){
-            this.currentIndex = sections.length - 1
-            this.animate = false
-        } 
+        // if(this.currentIndex < 0){
+        //     this.currentIndex = 0
+        //     this.animate = false
+        // }
+        // if(this.currentIndex >= sections.length){
+        //     this.currentIndex = sections.length - 1
+        //     this.animate = false
+        // } 
 
-        sections[this.currentIndex].addEventListener('transitionend',function callback(){
-            this.removeEventListener('transitionend',callback)
-            _this.animate = false
-        })
+        // sections[this.currentIndex].addEventListener('transitionend',function callback(){
+        //     this.removeEventListener('transitionend',callback)
+        //     _this.animate = false
+        // })
 
-        dom.every(sections, e => {
-            e.style.transform = `translateY(-${this.currentIndex}00%)`
+        // dom.every(sections, e => {
+        //     e.style.transform = `translateY(-${this.currentIndex}00%)`
+        // })
+        return new Promise((resolve,reject) => {
+            if(this.animate){
+                reject()
+            }else if(targetIndex < 0){
+                reject()
+            }else if(targetIndex >= this.options.element.children.length){
+                reject()
+            }else{
+                this.animate = true
+                let _this = this
+                
+                this.options.element.children[0].addEventListener('transitionend',function callback(){
+                    this.removeEventListener('transitionend',callback)
+                    _this.animate = false
+                    resolve()
+                })
+                
+                dom.every(this.options.element.children,section => {
+                    section.style.transform = `translateY(-${100*targetIndex}%)`
+                })
+            }
         })
     }
 
