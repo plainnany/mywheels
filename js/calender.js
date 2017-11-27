c4297f7a032fd6819a1728a3bbfb30ace92beda4
plainnany <plainnany@gmail.com>
2017-11-27 17:25:31 +0800

    finish generateWeekdays

--- js/calender.js
+++ js/calender.js
@@ -5,25 +5,34 @@ class Calender{
             dayTemplate: `
                 <li></li>
             `,
-            days: {0:'周日',1: '周一',2:'周二',3:'周三',4:'周四',5:'周五',6:'周六'},
+            weekdays: ['周日','周一','周二','周三','周四','周五','周六'],
+            day: n => n,
             output: date => `${date.getFullYear()}年${date.getMonth()+1}月`
         }
         this.options = Object.assign({},defaultOptions,options)
         this.currentDate = new Date()
-
+        this.generateWeekdays()
     }
     checkOptions(){
+        // 检查元素
         if(!this.options.element){
             throw new Error('element is required')
-        }
-        // 检查元素
+        }    
     }
     generateWeekdays(){
-
         // 产生周一到周日 添加到ol.weekdays中
+        let { weekdays } = this.options
+        let li = weekdays.map((value,index) => {
+            let n = (index >= 6 ? index + 1 - 7 : index + 1 )
+            return dom.create(`<li>${weekdays[n]}</li>`)
+        })
+        let ol = dom.create('<ol class="weekdays"></ol>',li)
+        this.options.element.appendChild(ol)
+        
     }
     generateCurrentMonth(){
-        // 产生当前月份的天数 
+        // 产生当前月份的所有天数 
+
     }
     generatePreviousMonth(){
         // 产生上个月的天数
@@ -38,4 +47,8 @@ class Calender{
         // 产生日历ol.days ol.weekdays 并append带div.calender中
     }
     
+}
+
+function createArray(){
+
 }
